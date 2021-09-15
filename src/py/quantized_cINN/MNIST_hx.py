bab29df80240e3af28a605761bf2d0c9d28c0445
Milena Czierlinski <milena.czierlinski@kip.uni-heidelberg.de>
2021-09-15 17:53:34 +0200

    Clean up hw example

--- src/py/quantized_cINN/MNIST_hx.py
+++ src/py/quantized_cINN/MNIST_hx.py
@@ -8,29 +8,31 @@ based on:   https://github.com/VLL-HD/conditional_INNs
 __all__ = ["CONFIG", "MNISTcINN_hx", "train", "evaluate"]
 
 # imports
-import numpy as np
 from collections import OrderedDict
+
+from time import time
+from tqdm import tqdm
+
+import numpy as np
 
 import torch
 import torch.optim
 import torch.nn as nn
 
-from tqdm import tqdm
-from time import time
-
 import FrEIA.framework as Ff
 import FrEIA.modules as Fm
 
-import hxtorch
-import _hxtorch
+from _hxtorch.constants import input_activation_max
 import hxtorch.nn as hxnn
 
 from common import one_hot, MNISTData, MNISTDataPreprocessed, baseCONFIG, \
-                   Visualizer, LiveVisualizer, sample_outputs, \
                    style_transfer, interpolation, val_loss, show_samples
+
+from layers import DynamicScaling, FixedRangeScaling
 
 ########################################################################
 # configuration
+
 
 class CONFIG(baseCONFIG):
     """
@@ -44,7 +46,7 @@ class CONFIG(baseCONFIG):
     maxpool = False
 
     img_size = (28, 28)
-    device = "cuda"
+    device = "cpu"
     n_workers = 4
 
     # Training
@@ -56,7 +58,7 @@ class CONFIG(baseCONFIG):
     milestones = [20, 40]
     betas = (0.9, 0.999)
 
-    n_epochs = 60
+    n_epochs = 20
 
     init_scale = 0.03
     pre_low_lr = 0
@@ -73,20 +75,19 @@ class CONFIG(baseCONFIG):
 
     # Logging/preview
     loss_names = ['L']
-    preview_upscale = 3                         # Scale up the images for preview
-    sampling_temperature = 0.8                  # Sample at a reduced temperature for the preview
-    progress_bar = True                         # Show a progress bar of each epoch
+    preview_upscale = 3         # Scale up the images for preview
+    sampling_temperature = 0.8  # Sample at a reduced temperature for preview
+    progress_bar = True         # Show a progress bar of each epoch
     eval_steps_interploation = 12
-    eval_seeds_interpolation  = (51, 89)
+    eval_seeds_interpolation = (51, 89)
 
     # Validation
     pca_weights = [
-        [(0,0.55)],
-        [(1,0.1), (3, 0.4), (4, 0.5)],
-        [(2,0.33), (3, 0.33), (1, -0.33)]]
+        [(0, 0.55)],
+        [(1, 0.1), (3, 0.4), (4, 0.5)],
+        [(2, 0.33), (3, 0.33), (1, -0.33)]]
     pca_gridsize = 10
     pca_extent = 8.
-
 
     # Paths
     mnist_data = "../../../mnist_data"
@@ -95,78 +96,82 @@ class CONFIG(baseCONFIG):
     load_file = "../../../out/MNIST_hx/mnist_hx_checkpoint.pt"
     filename = "../../../out/MNIST_hx/mnist_hx_cinn.pt"
 
-    checkpoint_save_interval =  20
+    checkpoint_save_interval = 1
     checkpoint_save_overwrite = True
     checkpoint_on_error = True
 
 ########################################################################
 # model definition
 
+
 class MNISTcINN_hx(nn.Module):
     """
     Conditional INN model for MNIST
     """
 
-    def __init__(self, config: object=CONFIG):
+    def __init__(self, config: object = CONFIG):
         super().__init__()
         self.c = config
 
         self.cinn = self.build_inn()
 
-        self.trainable_parameters = [p for p in self.cinn.parameters() if p.requires_grad]
-        for p in self.trainable_parameters:
-            p.data = self.c.init_scale * torch.randn_like(p)
+        self.trainable_parameters = []
+        for name, param in self.cinn.named_parameters():
+            if param.requires_grad and not name.split(".")[-2][-2:] == "__":
+                self.trainable_parameters.append(param)
+            continue
 
         self.cinn.to(self.c.device)
 
-        #gamma = (self.c.decay_by)**(1. / self.c.n_epochs)
         self.optimizer = torch.optim.Adam(self.trainable_parameters,
                                           lr=self.c.lr,
                                           weight_decay=self.c.weight_decay)
-        self.weight_scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer,
-                                                                #step_size=1,
-                                                                milestones=self.c.milestones,
-                                                                gamma=self.c.gamma)
+        self.weight_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            self.optimizer,
+            #step_size=1,
+            milestones=self.c.milestones,
+            gamma=self.c.gamma)
 
     def build_inn(self):
 
-        def scale_input_clamp(x_in: torch.Tensor) -> torch.Tensor:
+        def scale_input_total(x_in: torch.Tensor) -> torch.Tensor:
             """
             Scales the tensor to the maximal input range of BrainScaleS-2.
-            Clamps negative values to 0.
             """
-            max_in = torch.max(x_in)
-            factor = \
-                _hxtorch.constants.input_activation_max / max_in if max_in > 0 else 1
-            return torch.clamp(x_in * factor, min=0., max=31.)
+            max_abs = torch.max(torch.abs(x_in))
+            factor = input_activation_max / max_abs \
+                if max_abs != 0 else 1
+            return x_in * factor
 
         def fc_subnet(ch_in, ch_out):
-            #return nn.Sequential(nn.Linear(ch_in, self.c.internal_width),
-            #                     nn.ReLU(),
-            #                     nn.Linear(self.c.internal_width, ch_out))
-
-            return nn.Sequential(
-                hxnn.Linear(in_features=ch_in,
-                            out_features=self.c.internal_width,
-                            bias=True,
-                            num_sends=2,            # scales the hardware-gain, will be
-                                                    # adjusted automatically if set to None.
-                            wait_between_events=2,  # specifies wait time between two inputs
-                                                    # lower values may lead to saturation
-                                                    # effects in the drivers.
-                            mock=self.c.mock,       # enables simulation-mode.
-                            input_transform=scale_input_clamp),
-                hxnn.ConvertingReLU(shift=1,  # shifts the output by 1 bit,
-                                              # i.e. divides it by 2.
-                                    mock=self.c.mock),
-                hxnn.Linear(in_features=self.c.internal_width,
-                            out_features=ch_out,
-                            bias=True,
-                            num_sends=3,
-                            wait_between_events=2,
-                            mock=self.c.mock,
-                            input_transform=scale_input_clamp))
-
+            net = OrderedDict([
+                ("hx_input1", FixedRangeScaling(features=ch_in,
+                                                max_out=31,
+                                                per_feature=True)),
+                ("hx_lin_1", hxnn.Linear(in_features=ch_in,
+                                         out_features=self.c.internal_width,
+                                         bias=False,
+                                         num_sends=2,
+                                         wait_between_events=2,
+                                         mock=self.c.mock,
+                                         signed_input=True,
+                                         )),
+                ("relu1", hxnn.ConvertingReLU(shift=1,
+                                              mock=self.c.mock)),
+                ("hx_input2", FixedRangeScaling(features=self.c.internal_width,
+                                                max_out=input_activation_max,
+                                                per_feature=True)),
+                ("hx_lin_2", hxnn.Linear(in_features=self.c.internal_width,
+                                         out_features=ch_out,
+                                         bias=False,
+                                         num_sends=3,
+                                         wait_between_events=2,
+                                         mock=self.c.mock,
+                                         signed_input=True,
+                                         )),
+                ("hx_output", DynamicScaling(features=ch_out))
+            ])
+            return nn.Sequential(net)
 
         cond = Ff.ConditionNode(10)
 
@@ -185,11 +190,11 @@ class MNISTcINN_hx(nn.Module):
         nodes += [cond, Ff.OutputNode(nodes[-1])]
         return Ff.ReversibleGraphNet(nodes, verbose=False)
 
-    def forward(self, x, l, jac=True):
-        return self.cinn(x, c=one_hot(l, scale=31.), jac=jac)
+    def forward(self, x, label, jac=True):
+        return self.cinn(x, c=one_hot(label, scale=31.), jac=jac)
 
-    def reverse_sample(self, z, l, jac=True):
-        return self.cinn(z, c=one_hot(l, scale=31.), rev=True, jac=jac)
+    def reverse_sample(self, z, label, jac=True):
+        return self.cinn(z, c=one_hot(label, scale=31.), rev=True, jac=jac)
 
     def save(self, name):
         save_dict = {"opt": self.optimizer.state_dict(),
@@ -224,24 +229,24 @@ def train(config):
 
     nll_mean = []
 
-
     try:
         for i_epoch in range(-config.pre_low_lr, config.n_epochs):
             if i_epoch < 0:
                 for param_group in model.optimizer.param_groups:
                     param_group['lr'] = config.lr * 2e-2
 
-            for i_batch, (x, l) in tqdm(enumerate(data.train_loader),
-                                        total=len(data.train_loader),
-                                        leave=False,
-                                        mininterval=1.,
-                                        disable=(not config.progress_bar),
-                                        ncols=83):
+            for i_batch, (x, label) in tqdm(enumerate(data.train_loader),
+                                            total=len(data.train_loader),
+                                            leave=False,
+                                            mininterval=1.,
+                                            disable=(not config.progress_bar),
+                                            ncols=83):
 
-                x, l = x.cuda(), l.cuda()
-                z, log_j = model(x, l)
+                x, label = x.to(config.device), label.to(config.device)
+                z, log_j = model(x, label)
 
-                nll = torch.mean(z**2) / 2 - torch.mean(log_j) / np.prod(config.img_size)
+                nll = torch.mean(z**2) / 2 - torch.mean(log_j) / np.prod(
+                    config.img_size)
                 nll.backward()
                 torch.nn.utils.clip_grad_norm_(model.trainable_parameters,
                                                config.clip_grad_norm)
@@ -251,19 +256,19 @@ def train(config):
                 model.optimizer.step()
                 model.optimizer.zero_grad()
 
-                if not i_batch % 50:
-                    with torch.no_grad():
-                        z, log_j = model(data.val_x, data.val_l)
-                        nll_val = torch.mean(z**2) / 2 - torch.mean(log_j) / np.prod(config.img_size)
+            with torch.no_grad():
+                model.eval()
+                z, log_j = model(data.val_x, data.val_l)
+                nll_val = torch.mean(z**2) / 2 - torch.mean(log_j) / np.prod(
+                    config.img_size)
+                model.train()
 
-                    print('%.3i \t%.5i/%.5i \t%.2f \t%.6f\t%.6f\t%.2e' % (i_epoch,
-                                                                    i_batch, len(data.train_loader),
-                                                                    (time() - t_start)/60.,
-                                                                    np.mean(nll_mean),
-                                                                    nll_val.item(),
-                                                                    model.optimizer.param_groups[0]['lr'],
-                                                                    ), flush=True)
-                    nll_mean = []
+            print('%.3i \t%.5i/%.5i \t%.2f \t%.6f\t%.6f\t%.2e' %
+                  (i_epoch, i_batch, len(data.train_loader),
+                   (time() - t_start) / 60., np.mean(nll_mean), nll_val.item(),
+                   model.optimizer.param_groups[0]['lr'],
+                   ), flush=True)
+            nll_mean = []
 
             model.weight_scheduler.step()
 
@@ -271,7 +276,8 @@ def train(config):
             #    viz.update_losses(np.mean(nll_mean))
 
             if (i_epoch % config.checkpoint_save_interval) == 0:
-                model.save(config.filename + '_checkpoint_%.4i' % (i_epoch * (1-config.checkpoint_save_overwrite)))
+                model.save(config.filename + '_checkpoint_%.4i' %
+                           (i_epoch * (1-config.checkpoint_save_overwrite)))
 
         model.save(config.filename)
 
@@ -279,6 +285,7 @@ def train(config):
         if config.checkpoint_on_error:
             model.save(config.filename + "_ABORT")
         raise b
+
 
 def evaluate(config):
     model = MNISTcINN_hx(config)
@@ -289,33 +296,22 @@ def evaluate(config):
     else:
         data = MNISTData(config)
 
-    #for s in tqdm(range(0, 256)):
-    #    torch.manual_seed(s)
-    #    temperature(0.88, columns=1, save_as='./images/samples/T_%.4i.png' % (s))
-    #    plt.title(str(s))
-
+    train_config = f"width{config.internal_width}_epochs{config.n_epochs}"
 
     index_ins = [284, 394, 422, 759, 639, 599, 471, 449, 448, 426]
-    style_transfer(model, data, index_ins, config)
+    style_transfer(model, data, index_ins, config, train_config)
 
-    interpolation(model, config)
-
-    #for j in range(3):
-    #    plt.figure()
-    #    for i in range(10):
-    #        plt.subplot(10, 1, i+1)
-    #        val_set_pca(I=j, C=i)
-    #    plt.title(str(j))
+    interpolation(model, config, train_config)
 
     val_loss(model, data, config)
 
     for i in range(10):
-        show_samples(model, data, config, i)
-
+        show_samples(model, data, config, i, train_config)
 
 
 ########################################################################
 # execution
+
 
 if __name__ == "__main__":
     config = CONFIG()
--- src/py/quantized_cINN/layers.py
+++ src/py/quantized_cINN/layers.py
@@ -3,35 +3,33 @@ from typing import Dict
 
 import torch
 from torch import Tensor
-from torch.nn.parameter import Parameter, UninitializedParameter
+from torch.nn.parameter import Parameter
 from torch.nn import functional as F
 from torch.nn import init
 from torch.nn.modules import Module
 
 
 class DynamicScaling(Module):
-    r"""Applies a linear scaling to the incoming data: :math:`y = x*w + b`
-
+    r"""Applies a linear scaling to the incoming data:
+    :math:`y = x*w + b`
     This module supports :ref:`TensorFloat32<tf32_on_ampere>`.
-
     Args:
         features: size of each output sample
-        bias: If set to ``False``, the layer will not learn an additive bias.
-            Default: ``True``
-
+        bias: If set to ``False``, the layer will not learn an additive
+              bias. Default: ``True``
     Shape:
         - Input: :math:`(N, *, H)` where :math:`*` means any number of
           additional dimensions and :math:`H = \text{features}`.
         - Output: :math:`(N, *, H)`.
-
     Attributes:
         weight: the learnable weights of the module of shape
             :math:`(\text{features})`. The values are
-            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
-            :math:`k = \frac{1}{\text{in\_features}}`
-        bias:   the learnable bias of the module of shape :math:`(\text{features})`.
-                If :attr:`bias` is ``True``, the values are initialized from
-                :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
+            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`,
+            where :math:`k = \frac{1}{\text{in\_features}}`
+        bias:   the learnable bias of the module of shape
+                :math:`(\text{features})`.
+                If :attr:`bias` is ``True``, the values are initialized
+                from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
                 :math:`k = \frac{1}{\text{in\_features}}`
     """
     __constants__ = ['features']
@@ -39,7 +37,8 @@ class DynamicScaling(Module):
     weight: Tensor
 
     def __init__(self, features: int, bias: bool = True,
-                device=None, dtype=None, init: Dict[str, float]=None) -> None:
+                 device=None, dtype=None, init: Dict[str, float] = None) \
+            -> None:
         factory_kwargs = {'device': device, 'dtype': dtype}
         super(DynamicScaling, self).__init__()
         self.features = features
@@ -58,26 +57,41 @@ class DynamicScaling(Module):
     def reset_parameters(self) -> None:
         init.kaiming_uniform_(torch.diag(self.weight), a=math.sqrt(5))
         if self.bias is not None:
-            fan_in, _ = init._calculate_fan_in_and_fan_out(torch.diag(self.weight))
+            fan_in, _ = init._calculate_fan_in_and_fan_out(
+                torch.diag(self.weight))
             bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
             init.uniform_(self.bias, -bound, bound)
 
     def reset_parameters_static(self, init: Dict[str, float]):
-        for p, v in init.items():
-            nn.init._no_grad_fill_(getattr(self, p), v)
+        for param, value in init.items():
+            init._no_grad_fill_(getattr(self, param), value)
 
-    def forward(self, input: Tensor) -> Tensor:
-        return F.linear(input, torch.diag(self.weight), self.bias)
-
+    def forward(self, input_: Tensor) -> Tensor:
+        return F.linear(input_, torch.diag(self.weight), self.bias)
 
     def extra_repr(self) -> str:
         return 'features={}, bias={}'.format(
-            self.features, self.bias is not None
-        )
+            self.features, self.bias is not None)
 
 
 class FixedRangeScaling(Module):
-    r"""
+    r"""Scales the incoming data to an specified absolute value range.
+    This module supports :ref:`TensorFloat32<tf32_on_ampere>`.
+    Args:
+        features: size of each output sample
+        max_out: maximal absolute value of outgoing data
+        per_feature: If set to ``False``, the value ratio between
+                     features is preserved.
+                     Default: ``True``
+    Shape:
+        - Input: :math:`(N, *, H)` where :math:`*` means any number of
+          additional dimensions and :math:`H = \text{features}`.
+        - Output: :math:`(N, *, H)`.
+    Attributes:
+        weight: the learnable weights of the module of shape
+            :math:`(\text{features})`. The values are
+            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
+            :math:`k = \frac{1}{\text{in\_features}}`
     """
     __constants__ = ['features', 'max_out']
     per_feature: bool
@@ -100,17 +114,22 @@ class FixedRangeScaling(Module):
     def reset_parameters(self) -> None:
         init.ones_(self.weight)
 
-    def forward(self, input: Tensor) -> Tensor:
+    def forward(self, input_: Tensor) -> Tensor:
         if self.training:
-            max_abs, _ = torch.max(torch.abs(input), axis=0)
+            max_abs, _ = torch.max(torch.abs(input_), axis=0)
             if not self.per_feature:
                 max_abs.fill_(max_abs.max())
             max_abs[max_abs == 0.] = self.max_out
-            with torch.no_grad:
+            with torch.no_grad():
                 self.weight.data = self.max_out / max_abs
-        return F.linear(input, torch.diag(self.weight))
+        res = F.linear(input_, torch.diag(self.weight))
+        res[res > 0] = torch.floor(res[res > 0])
+        res[res < 0] = torch.ceil(res[res < 0])
+        # FIXME: This is needed for the last batch. Why? -> debug
+        if torch.max(res).data.item() > 31 or torch.min(res).data.item() < -31:
+            print("clamping", torch.max(res).data.item())
+        return torch.clamp(res, min=-31, max=31)
 
     def extra_repr(self) -> str:
         return 'features={}, max_out={}, per_feature={}'.format(
-            self.features, self.max_out, self.per_feature
-        )
+            self.features, self.max_out, self.per_feature)
